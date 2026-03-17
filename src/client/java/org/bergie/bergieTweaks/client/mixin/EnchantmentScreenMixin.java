5ac61db3dde7b4320a1c0c4cd9bd7bb6d7e1b02c
Alex Bergman <alexbergman40@gmail.com>
2026-03-16 18:06:48 -0700

    Fixed enchantment screen gui

--- src/client/java/org/bergie/bergieTweaks/client/mixin/EnchantmentScreenMixin.java
+++ src/client/java/org/bergie/bergieTweaks/client/mixin/EnchantmentScreenMixin.java
@@ -1,21 +1,21 @@
 package org.bergie.bergieTweaks.client.mixin;
 
 import com.mojang.blaze3d.pipeline.RenderPipeline;
-import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.ingame.EnchantmentScreen;
-import net.minecraft.text.StringVisitable;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.EnchantmentScreen;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 
-@Mixin(EnchantmentScreen.class)
+@Mixin(net.minecraft.client.gui.screens.inventory.EnchantmentScreen.class)
 abstract public class EnchantmentScreenMixin {
     @Redirect(
             method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/screen/ingame/EnchantmentScreen;isPointWithinBounds(IIIIDD)Z"
+                    target = "Lnet/minecraft/client/gui/screens/inventory/EnchantmentScreen;isHovering(IIIIDD)Z"
             )
     )
     private boolean noClue(EnchantmentScreen instance, int i, int j, int k, int l, double v, double w)
@@ -24,41 +24,41 @@ abstract public class EnchantmentScreenMixin {
     }
 
     @Redirect(
-            method = "drawBackground",
+            method = "renderBg",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIII)V",
+                    target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIII)V",
                     ordinal = 2
             )
     )
-    private void noLevelDisabled(DrawContext instance, RenderPipeline pipeline, Identifier sprite, int x, int y, int width, int height)
+    private void noLevelDisabled(GuiGraphics instance, RenderPipeline pipeline, Identifier sprite, int x, int y, int width, int height)
     {}
 
     @Redirect(
-            method = "drawBackground",
+            method = "renderBg",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIII)V",
+                    target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIII)V",
                     ordinal = 5
             )
     )
-    private void noLevelEnabled(DrawContext instance, RenderPipeline pipeline, Identifier sprite, int x, int y, int width, int height)
+    private void noLevelEnabled(GuiGraphics instance, RenderPipeline pipeline, Identifier sprite, int x, int y, int width, int height)
     {}
 
     @Redirect(
-            method = "drawBackground",
+            method = "renderBg",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/DrawContext;drawWrappedText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/StringVisitable;IIIIZ)V"
+                    target = "Lnet/minecraft/client/gui/GuiGraphics;drawWordWrap(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/FormattedText;IIIIZ)V"
             )
     )
-    private void shiftEnchantingLanguage(DrawContext instance, TextRenderer textRenderer, StringVisitable text, int x, int y, int width, int color, boolean shadow)
+    private void shiftEnchantingLanguage(GuiGraphics instance, Font font, FormattedText text, int x, int y, int width, int color, boolean shadow)
     {
-        instance.drawWrappedText(textRenderer, text, x - 18, y, width, color, shadow);
+        instance.drawWordWrap(font, text, x - 18, y, width, color, shadow);
     }
 
     @ModifyVariable(
-            method = "drawBackground",
+            method = "renderBg",
             at = @At("STORE"),
             ordinal = 9
     )
